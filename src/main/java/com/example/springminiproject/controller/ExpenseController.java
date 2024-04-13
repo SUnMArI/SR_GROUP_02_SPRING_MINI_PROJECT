4cfdb2077307f5ca5f39e5357005a0a4cb2f81e4
SUnMArI <thy.sopheak098@gmail.com>
2024-04-13 21:52:45 +0700

    add new dependency

--- src/main/java/com/example/springminiproject/controller/ExpenseController.java
+++ src/main/java/com/example/springminiproject/controller/ExpenseController.java
@@ -1,4 +1,13 @@
 package com.example.springminiproject.controller;
 
+import com.example.springminiproject.service.ExpenseService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/expense")
 public class ExpenseController {
+    private final ExpenseService expenseService;
 }
